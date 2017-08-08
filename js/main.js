a41a98e2f715d9024a208a04053681e11b115f60
gabriela pappova <gpgabika@gmail.com>
2017-08-09 00:19:56 +0100

    jquery

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
- function updateOutput() {
-
- 	$("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
-
- 	document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
- }
- $(".toggleButton").hover(function () {
-
+ 
+  $(".toggleButton").hover(function(){
  	$(this).addClass("highlightedButton");
 
- }, function () {
-
+ }, function() {
  	$(this).removeClass("highlightedButton");
-
  });
- $(".toggleButton").click(function () {
+
+  $(".toggleButton").click(function () {
 
  	$(this).toggleClass("active");
 
  	$(this).removeClass("highlightedButton");
 
  	var panelId = $(this).attr("id") + "Panel";
-
  	$("#" + panelId).toggleClass("hidden");
-
  	var numberOfActivePanels = 4 - $('.hidden').length;
 
- 	$(".panel").width(($(window).width() / numberOfActivePanels) - 15);
+ 	$(".panel").width(($(window).width() / numberOfActivePanels) - 25);
+});
 
- })
- $(".toggleButton").hover(function(){
- 	$(this).addClass("highlightedButton");
 
- }, function() {
- 	$(this).removeClass("highlightedButton");
- });
+  function updateOutput() {
+
+ 	$("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+ 	document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
+ }
+
+ 
+
+
 
  $(".panel").height($(window).height()-$("#header").height()- 15);
  $(".panel").width(($(window).width()/2)-10);
