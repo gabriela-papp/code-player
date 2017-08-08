facae36043849eb6de6a7457066479b6ba612668
gabriela pappova <gpgabika@gmail.com>
2017-08-08 23:27:36 +0100

    some styling added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 
+$(".toggleButton").hover(function(){
+	$(this).css("background-color", "grey");
 	
+}, function() {
+	$(this).css("background-color", "#EEEEEE");
+});
+
+$(".panel").height($(window).height()-$("#header").height()- 15);
+$(".panel").width(($(window).width()/2)-10);
